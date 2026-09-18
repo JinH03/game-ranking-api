faf518df8cc5f9ff512769d5f76fcf47cd6322ec
JinH03 <03jinhyung@gmail.com>
2026-09-18 22:43:04 +0900

    쿼리, 패스 파라미터 학습 및 특정 플레이어 조회

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from fastapi import FastAPI
-
+from fastapi import HTTPException, Query
 app = FastAPI()
 
+game_scores = [
+    {"name": "Player1", "rating": 1500},
+    {"name": "Player2", "rating": 1200},
+    {"name": "Player3", "rating": 900}
+]
 
 @app.get("/")
 def root():
-    return {"message":"hello World!"}
+    return game_scores
 
 @app.get("/scores")
-def scores():
-    return [
-        {"name":"Player1","rating":1500},
-        {"name":"Player2","rating":1200},
-        {"name":"Player3","rating":900}
-    ]
+def scores(limit: int = Query(3, ge=1, le=3)):
+    return game_scores[:limit]
+
+@app.get("/scores/{player_name}")
+def get_score(player_name):
+    for player in game_scores():
+        if player["name"] == player_name:
+            return player
+    raise HTTPException(status_code=404, detail="Player not Found")
+##uvicorn main:app --reload
